6c6bc8304d86c08f848aa2fe235f42dc2c4eb4c4
Casey Bui <buikhacnam11@gmail.com>
2022-10-08 05:46:22 +0700

    initial commit

---
+++ src/main/java/com/buinam/entitymanagerexample/EntityManagerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.buinam.entitymanagerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityManagerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityManagerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buinam/entitymanagerexample/EntityManagerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buinam.entitymanagerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityManagerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
